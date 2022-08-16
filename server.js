c5cecbcbd3b08d998e2582f0806861b29e192ddf
张圣 <zhangsheng@fineyun.cn>
2022-08-16 16:41:45 +0800

    node服务器

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const server = express()
+const fs = require('fs')
+// 监听端口
+server.listen(1818, err => {
+    err || console.log("服务器启动成功")
+})
+// 部署网页
+server.use(express.static(__dirname + "/dist"))
+// get请求
+server.get("/get", (req, res) => {
+    res.send({
+        name: "张三"
+    })
+})
+// AdGuard广告规则
+server.get("/AdGuard", (req, res) => {
+    fs.readFile("./filter.txt", 'utf8', (err, file) => {
+        res.send(file)
+    })
+})
