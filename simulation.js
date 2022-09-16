11b955ae0469e3bafae9a0d817c2274255a0c476
张圣 <zhangsheng@fineyun.cn>
2022-09-16 17:02:09 +0800

    模拟接口

--- simulation.js
+++ simulation.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 server.listen(4399, (err) => {
-  err || console.log(err);
+  err || console.log("待机中。。。");
 });
 server.all("*", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
