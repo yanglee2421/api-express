8a716bdceda8efb05c7e3f5cfce1488374d17754
李又土 <xtcff082421@gmail.com>
2022-10-24 21:07:17 +0800

    路径变化

--- src/hook/useLog.js
+++ src/hook/useLog.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 module.exports = (reqPath) => {
   fs.writeFile(
-    path.resolve(__dirname, "../log/log.txt"),
+    path.resolve(__dirname, "../../log/log.txt"),
     `${new Date()}，请求了：${reqPath}\n`,
     { flag: "a" },
     (err) => {
