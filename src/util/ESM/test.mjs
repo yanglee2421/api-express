bcbe40a9ab512a56efa0ee03a38bcb2addb575fb
李又土 <xtcff082421@gmail.com>
2022-12-10 13:59:28 +0800

    new 基本类型的坑

--- src/util/ESM/test.mjs
+++ src/util/ESM/test.mjs
@@ -1,1 +1,7 @@
-import "./mod.mjs";
+const target = new Boolean(false);
+console.log(target);
+const targetType = Object.prototype.toString.call(target);
+console.log("typeof", typeof target);
+console.log("strTag", target || targetType);
+console.log("==", target == false);
+console.log("===", target === false);
